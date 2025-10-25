91850d8abf1a0d879a9190f06d3f47599d16bb67
Fuad Farhan <fuadhoquef8414@hstat.org>
2025-10-24 23:00:27 -0400

    page front end starter

--- hunterevents/src/pages/homepage.js
+++ hunterevents/src/pages/homepage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import '../App.css';
 
 
 export default function Home() {
-  return <h1>Welcome to Home Page</h1>;
+  return (
+    <div>
+      <h1 className="title">Hunter Daily Events</h1>
+
+      <div className="event-cards">
+        <div className="event-text">
+          <h3>Hunter Events</h3>
+          <p>hunter event details</p>
+
+        </div>
+
+        
+
+      </div>
+    </div>
+  )
 } 
 
